ebfb0f1f455952e1d5bbb167553bf9169f5bc50a
Haymanot W <haymanotalemu21@gmail.com>
2020-10-15 11:41:35 -0400

    header component started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/pages/Home";
+import Resume from "./components/pages/Resume";
+import Projects from "./components/pages/Projects";
+import Footer from "./components/Footer";
 
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+    </Router>
+    </>
   );
 }
 
