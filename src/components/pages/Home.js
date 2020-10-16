61121cc0cc03bffbc3387b7861143e94b4abb43d
Haymanot W <haymanotalemu21@gmail.com>
2020-10-16 14:12:03 -0400

    projects json data added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,1 +1,21 @@
-
+import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import Card from "../Card";
+ function Home(){
+    return(
+        <div>
+            <Container>
+                <Row>
+                    <Col size="md-3">
+                    </Col>
+                    <Col size="md-6">
+                        <Card/>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+ }
+export default Home;
