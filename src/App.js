d0daa68591561deaf6779ca682b8ad94917f5e14
Haymanot W <haymanotalemu21@gmail.com>
2020-10-16 18:01:37 -0400

    home page components created

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/resume" component={Resume} />
         </Switch>
+
         <Footer />
     </Router>
     </>
--- src/components/Wrapper.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Warpper.css";
-
-function Wrapper(props) {
-    return <main className="wrapper">{props.children}</main>;
-}
-
-export default Wrapper;
