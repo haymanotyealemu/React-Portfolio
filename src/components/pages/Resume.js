2a2055643e3a56c952e1708afdb5bbc1b1b400ef
Haymanot W <haymanotalemu21@gmail.com>
2021-02-01 12:11:03 -0500

    updated

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "../../components/Container";
 import Row from "../../components/Row";
 import Col from "../../components/Col";
-import resume from "../../Haymanot Wolde.pdf";
+import resume from "../../Haymanot Wolde .pdf";
 
 
 function Resume(){
